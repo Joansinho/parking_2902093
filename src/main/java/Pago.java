3e328da4fe136f3a7b6f488ac57adc02a57a06c9
angiessosa <assosa2@soy.sena.edu.co>
2024-02-26 14:22:28 -0500

    Entidades listas para el main

--- src/main/java/Pago.java
+++ src/main/java/Pago.java
@@ -1,3 +1,38 @@
-public class Pago {
+
+import java.time.LocalDateTime;
+
+import org.parking.java.entities.Cupo;
+import org.parking.java.entities.Vehiculo;
+
+
+  public class Pago { 
+    
+  // Atríbutos Intrsínsecos
+   
+    public LocalDateTime fechaHoraInicio;
+    public LocalDateTime fechaHoraFin;
+    public Double valor;
+     
+    //Atributos de relación
+
+    public Vehiculo vehiculo; 
+    public Cupo cupo;
+   
+
+    //constructor por defecto 
+    public Pago() {
+    }
+   //Constructor parametrizado
+
+    public Pago(LocalDateTime fechaHoraInicio, LocalDateTime fechaHoraFin, Double valor, Vehiculo vehiculo, Cupo cupo) {
+        this.fechaHoraInicio = fechaHoraInicio;
+        this.fechaHoraFin = fechaHoraFin;
+        this.valor = valor;
+        this.vehiculo = vehiculo;
+        this.cupo = cupo;
+    }
+    
+    
+
 
 }
